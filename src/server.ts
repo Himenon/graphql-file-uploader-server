5fff0ab736d07f06e377c0a6ff7ed17c6d0634f8
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-07-03 22:54:06 +0900

    chore: update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import cors from "cors";
 import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { buildSchema } from "graphql";
+
+const PORT = 8000;
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
 const app = express();
 
-app.use(cors());
+const root = {
+  hello() {
+    return "Hello world!";
+  },
+};
 
-const server = app.listen(PORT, () => {
+app.use(cors());
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 });
